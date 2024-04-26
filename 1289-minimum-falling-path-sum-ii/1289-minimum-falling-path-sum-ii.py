90fc0b614032121453f619302268783f6a17002d
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-04-26 17:35:32 -0400

    Time: 649 ms (40.09%), Space: 20.1 MB (34.8%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.py
@@ -1,6 +1,6 @@
 class Solution:
     def minFallingPathSum(self, grid: List[List[int]]) -> int:
-        
+
         rows, cols = len(grid), len(grid[0])
 
         dp = [[0 for j in range(cols)] for i in range(rows)]
@@ -8,10 +8,8 @@ class Solution:
 
         for i in range(1, rows):
             for j in range(cols):
-                left = min(dp[i-1][:j]) if j != 0 else float("inf")
-                right = min(dp[i-1][j+1:]) if j != cols - 1 else float("inf")
+                left = min(dp[i - 1][:j]) if j != 0 else float("inf")
+                right = min(dp[i - 1][j + 1:]) if j != cols - 1 else float("inf")
                 dp[i][j] = grid[i][j] + min(left, right)
-        
+
         return min(dp[-1])
-
-
