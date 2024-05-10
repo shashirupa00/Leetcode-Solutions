4902bece45ab3cc93543d350de55d582ba14dc9b
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-05-10 12:37:52 -0400

    Time: 946 ms (49.83%), Space: 68.8 MB (50.51%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthSmallestPrimeFraction(self, arr: List[int], k: int) -> List[int]:
+
+        res = []
+
+        for i in range(len(arr)):
+            for j in range(i+1, len(arr)):
+                res.append((arr[i]/arr[j], arr[i], arr[j]))
+        
+        res.sort()
+        
+        return [res[k-1][1], res[k-1][2]]
+                
