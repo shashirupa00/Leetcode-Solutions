0df74f87766e32b0755f732ac9ad43f6ce0b1d4c
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-05-24 17:12:16 -0400

    Time: 609 ms (40.51%), Space: 16.7 MB (96.84%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.py
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.py
@@ -5,8 +5,7 @@ class Solution:
         
         for i in range(len(nums) - 2, -1, -1):
             for j in range(i+1, len(nums)):
-                if abs(nums[i] - nums[j]) <= target:
-                    if dp[j] != 0 or j == len(nums) - 1:
-                        dp[i] = max(dp[i], 1 + dp[j])
+                if (dp[j] != 0 or j == len(nums) - 1) and abs(nums[i] - nums[j]) <= target:
+                    dp[i] = max(dp[i], 1 + dp[j])
         
         return dp[0] if dp[0] else -1
