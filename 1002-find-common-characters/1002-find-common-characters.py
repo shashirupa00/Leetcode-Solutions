5f4462121355cb3288a15276dde39c12540be9d5
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-06-05 12:44:13 -0400

    Time: 48 ms (64.28%), Space: 17 MB (8.86%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -0,0 +1,28 @@
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        
+        arr = []
+        setList = []
+        letters = []
+        res = []
+
+        for word in words:
+            arr.append(Counter(word))
+            temp = set()
+            for letter in word:
+                temp.add(letter)
+            setList.append(temp)
+        
+        common = set.intersection(*setList)
+        
+        for letter in common:
+            temp = [letter, float("inf")]
+            for word in arr:
+                temp[1] = min(temp[1], word[letter])
+            x = [letter] * temp[1]
+            res.extend(x)
+
+        return res
+            
+
+        
