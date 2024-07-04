7598752ca9261a1681a7aea3ea35744ba1d119f8
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-07-04 11:27:53 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1820-maximum-number-of-accepted-invitations/1820-maximum-number-of-accepted-invitations.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumInvitations(self, grid: List[List[int]]) -> int:
+        
+        rows, cols = len(grid), len(grid[0])
+        hashMap = {}
+        visited = set()
+
+        def backTrack(i):
+            
+            for j in range(cols):
+
+                if grid[i][j] and j not in visited:
+                    visited.add(j)
+
+                    if j not in hashMap or backTrack(hashMap[j]):
+                        hashMap[j] = i
+                        return True
+                    
+                    visited.remove(j)
+            return
+        
+
+        for i in range(rows):
+            visited = set()
+            backTrack(i)
+        
+        return len(hashMap)
