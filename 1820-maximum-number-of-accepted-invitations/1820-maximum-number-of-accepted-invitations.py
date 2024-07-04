d5dc0bd80226ae0d5082ddc36f49a43c654346a5
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-07-04 11:29:06 -0400

    Time: 981 ms (15.79%), Space: 17.6 MB (97.37%) - LeetHub

--- 1820-maximum-number-of-accepted-invitations/1820-maximum-number-of-accepted-invitations.py
+++ 1820-maximum-number-of-accepted-invitations/1820-maximum-number-of-accepted-invitations.py
@@ -3,22 +3,25 @@ class Solution:
         
         rows, cols = len(grid), len(grid[0])
         hashMap = {}
+        visited = set()
 
-        def backTrack(i, visited):
+        def backTrack(i):
             
             for j in range(cols):
 
                 if grid[i][j] and j not in visited:
                     visited.add(j)
 
-                    if j not in hashMap or backTrack(hashMap[j], visited):
+                    if j not in hashMap or backTrack(hashMap[j]):
                         hashMap[j] = i
                         return True
                     
+                    # visited.remove(j)
             return
         
 
         for i in range(rows):
-            backTrack(i, set())
+            visited = set()
+            backTrack(i)
         
         return len(hashMap)
