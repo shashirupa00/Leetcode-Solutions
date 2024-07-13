b05667b49b9ed5ba539141c19f1ce985cc7e51fa
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-07-13 19:19:47 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.py
@@ -0,0 +1,29 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        
+        letterLogs = []
+        digitLogs = []
+
+        def checkLog(log):
+            
+            if log[-1].isalpha():
+                return 'letter'
+            
+            return 'digit'
+        
+        for log in logs:
+            
+            logType = checkLog(log)
+            
+            if logType == 'letter':
+                letterLogs.append(log)
+            else:
+                digitLogs.append(log)
+
+        def sortKeys(log):
+            identifier, content = log.split(maxsplit=1)
+            return content
+        
+        letterLogs = sorted(letterLogs, key=sortKeys)
+
+        return letterLogs + digitLogs
