5503dbe2d84b73ea52fdd4d22e71f0547548d8df
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-07-22 12:33:16 -0400

    Time: 688 ms (11.77%), Space: 78.4 MB (17.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,49 +1,54 @@
 class Node:
     def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
-
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
 
 class LRUCache:
 
     def __init__(self, capacity: int):
+
         self.cap = capacity
-        self.hashMap = {}
+        self.cache = {}
 
-        self.head, self.tail = Node(0,0), Node(0,0)
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
 
-        #   head -> Most recently used, tail -> LRU
-        self.head.next, self.tail.prev = self.tail, self.head
-
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    def insert(self, node):
-        prv, nxt = self.tail.prev, self.tail
-        prv.next = nxt.prev = node
-        node.next, node.prev = nxt, prv
+    def remove(self, Node):
+        prevNode, nxtNode = Node.prev, Node.next
+        prevNode.next, nxtNode.prev = nxtNode, prevNode
+        
+    def insert(self, Node):
+        prevNode, nxtNode = self.right.prev, self.right
+        prevNode.next, nxtNode.prev = Node, Node
+        Node.prev, Node.next = prevNode, nxtNode
 
     def get(self, key: int) -> int:
-        if key in self.hashMap:
-            self.remove(self.hashMap[key])
-            self.insert(self.hashMap[key])            
-            return self.hashMap[key].val    
-        return -1        
+
+        if key in self.cache:
+
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+
+            return self.cache[key].val
+        
+        return -1
 
     def put(self, key: int, value: int) -> None:
 
-        if key in self.hashMap:
-            self.remove(self.hashMap[key])
-        self.hashMap[key] = Node(key, value)
-        self.insert(self.hashMap[key])
+        if key in self.cache:
+            self.cache[key].val = value
+            self.remove(self.cache[key])
 
-        if len(self.hashMap) > self.cap:
-            node = self.head.next
-            self.remove(node)
-            del self.hashMap[node.key]
+        else:
+            self.cache[key] = Node(key, value)
+        
+        self.insert(self.cache[key])
 
-            
+        if len(self.cache) > self.cap:
+            del self.cache[self.left.next.key]
+            self.remove(self.left.next)
 
         
 
