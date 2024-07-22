55344ca3299671d5183f785953d86227d4124174
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-07-22 13:35:38 -0400

    Time: 196 ms (48.15%), Space: 23 MB (33.25%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -3,33 +3,27 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-
+        
+        rows, cols = len(rooms), len(rooms[0])
         deq = collections.deque([])
-        rows, cols = len(rooms),len(rooms[0])
-        directions =[[0,1],[1,0],[-1,0],[0,-1]]
         visited = set()
-        INF = 2147483647
 
         for i in range(rows):
             for j in range(cols):
                 if rooms[i][j] == 0:
-                    deq.append((i,j))
-                    visited.add((i,j))
-
+                    deq.append((i, j, 0))
+                    visited.add((i, j))
+        
         while deq:
             for _ in range(len(deq)):
-                a, b = deq.popleft()
 
-                for x, y in directions:
-                    r, c = a+x, b+y
+                x, y, dist = deq.popleft()
 
-                    if (r not in range(rows) or
-                    c not in range(cols) or
-                    (r,c) in visited or
-                    rooms[r][c] != INF):
-
-                        continue
-                    
-                    rooms[r][c] = rooms[a][b] + 1
-                    deq.append((r,c))
-                    visited.add((r,c))
+                for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+                    nx, ny = x + dx, y + dy
+                    if (0 <= nx < rows and 0 <= ny < cols and  
+                    (nx, ny) not in visited and rooms[nx][ny] != -1):
+                        rooms[nx][ny] = dist + 1
+                        deq.append((nx, ny, dist + 1))
+                        visited.add((nx, ny))
+        
