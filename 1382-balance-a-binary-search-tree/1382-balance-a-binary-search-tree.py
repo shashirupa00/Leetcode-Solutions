a8dede95622366d71c02d0d64e7bdba578b9a1d3
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-07-23 12:17:26 -0400

    Time: 202 ms (51.04%), Space: 22.3 MB (6.97%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.py
@@ -6,37 +6,31 @@
 #         self.right = right
 class Solution:
     def balanceBST(self, root: TreeNode) -> TreeNode:
-        
-        arr = []
+
+        inOrder = []
 
         def dfs(node):
-
+            
             if not node:
                 return
             
             dfs(node.left)
-            arr.append(node.val)
+            inOrder.append(node.val)
             dfs(node.right)
-
-            return
-
-        dfs(root)
-
-        def createTree(start, end):
-
-            if start > end:
-                return
-
-            pos = (start + end) // 2
-
-            if start == end:
-                return TreeNode(arr[pos])
-            
-            cur = TreeNode(arr[pos])
-
-            cur.left = createTree(start, pos-1)
-            cur.right = createTree(pos+1, end)
-
-            return cur
         
-        return createTree(0, len(arr)-1)        
+        dfs(root)
+        
+        def buildTree(low, high):
+
+            if low >= high:
+                return None
+            
+            mid = (low + high) // 2
+            curNode = TreeNode(inOrder[mid])
+
+            curNode.left = buildTree(low, mid)
+            curNode.right = buildTree(mid + 1, high)
+
+            return curNode
+        
+        return buildTree(0, len(inOrder))
