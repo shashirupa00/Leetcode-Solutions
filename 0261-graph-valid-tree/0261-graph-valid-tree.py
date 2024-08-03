49685b0cca19926f79c412b839eaed067e7de095
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-08-03 19:17:27 -0400

    Time: 91 ms (31.32%), Space: 18.4 MB (50.95%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,33 +1,22 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
 
-        par = [i for i in range(n)]
-        rank = [1 for i in range(n)]
+        if len(edges) != n - 1: return False
+    
+        hashMap = defaultdict(list)
+        visited = set()
 
-        def find(p):
-            while p != par[p]:
-                p = par[p]
-            return p
-
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-
-            if p1 == p2:
-                return
-            
-            elif rank[p1] >= rank[p2]:
-                par[p2] = p1
-                rank[p1] += rank[p2]
-            
-            else:
-                par[p1] = p2
-                rank[p2] += rank[p1]
+        for src, dst in edges:
+            hashMap[src].append(dst)
+            hashMap[dst].append(src)
         
-        for a,b in edges:
-            union(a,b)
-        
-        for i in range(len(par)):
-            par[i] = find(par[i])       
+        def dfs(node):
+            
+            if node in visited: return
+            visited.add(node)
+            for nxt in hashMap[node]:
+                dfs(nxt)
 
+        dfs(0)
 
-        return len(set(par)) == 1 and n-1 == len(edges)
+        return len(visited) == n
