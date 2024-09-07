0920295ca664998465bbd15e6365010ac5ccd601
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-06 20:50:08 -0400

    Time: 300 ms (18.48%), Space: 27.9 MB (6.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,35 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-
-        visited = set()
-        islands = 0
+        
         rows, cols = len(grid), len(grid[0])
-        deq = collections.deque([])
+        res = 0
+        visited = set()
 
+        def dfs(i, j):
+
+            if i not in range(rows) or j not in range(cols) or (i, j) in visited or grid[i][j] == '0':
+                return
+            
+            visited.add((i, j))
+
+            dfs(i + 1, j)
+            dfs(i, j + 1)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
+
+            return
+        
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == "1" and (i,j) not in visited: 
-                    deq.append((i,j))
-                    while deq:
-                        x, y = deq.popleft()
-
-                        directions = [[1,0],[0,1],[-1,0],[0,-1]]
-                        
-                        for k in range(len(directions)):
-                            r, c = x+directions[k][0], y+directions[k][1]
-
-                            if (
-                                0<=r<=rows-1
-                                and 0<=c<=cols-1
-                                and grid[r][c] == "1"
-                                and (r, c) not in visited
-                            ):
-
-                                deq.append((r,c))
-                                visited.add((r,c))
-
-                    islands += 1
-    
-        return islands
-
-            
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    res += 1
+                    dfs(i, j)
+        
+        return res
