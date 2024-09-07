cb7294b6e16106acb4e15d14874b82adb22a085a
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-07 18:51:38 -0400

    Time: 58 ms (41.94%), Space: 17 MB (85.6%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -4,30 +4,28 @@ class Solution:
         par = [i for i in range(len(edges) + 1)]
         rank = [1 for i in range(len(edges) + 1)]
 
-        def findParent(p):
-            
-            while par[p] != p:
-                p = par[p]
-            
-            return p
+        def findParent(node):
+            while node != par[node]:
+                node = par[node]
+            return node
         
-        def unionFind(node1, node2):
+        def unionFind(n1, n2):
             
-            parent1, parent2 = findParent(node1), findParent(node2)
+            p1, p2 = findParent(n1), findParent(n2)
 
-            if parent1 == parent2:
+            if p1 == p2:
                 return False
-
-            if rank[parent1] >= rank[parent2]:
-                par[parent2] = parent1
-                rank[parent1] += rank[parent2]
+            
+            if rank[p1] > rank[p2]:
+                rank[p1] += rank[p2]
+                par[p2] = p1
             
             else:
-                par[parent1] = parent2
-                rank[parent2] += rank[parent1]
-
+                rank[p2] += rank[p1]
+                par[p1] = p2
+            
             return True
-
+        
         for a, b in edges:
             if not unionFind(a, b):
                 return [a, b]
