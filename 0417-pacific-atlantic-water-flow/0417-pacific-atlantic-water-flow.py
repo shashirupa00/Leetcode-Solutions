3c2a79002e445136effd106f3ac4083d3f452b9c
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-06 23:26:42 -0400

    Time: 200 ms (90.5%), Space: 18.2 MB (60.49%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -5,27 +5,26 @@ class Solution:
         pacificSet = set()
         atlanticSet = set()
 
-        def dfs(i, j, ocean, visited, prev):
+        def dfs(i, j, visited, prev):
             
             if not (0 <= i < rows) or not (0 <= j < cols) or (i, j) in visited or heights[i][j] < prev:
                 return
-
-            ocean.add((i, j))
+            
             visited.add((i, j))
             
-            dfs(i + 1, j, ocean, visited, heights[i][j])
-            dfs(i, j + 1, ocean, visited, heights[i][j])
-            dfs(i - 1, j, ocean, visited, heights[i][j])
-            dfs(i, j - 1, ocean, visited, heights[i][j])
+            dfs(i + 1, j, visited, heights[i][j])
+            dfs(i, j + 1, visited, heights[i][j])
+            dfs(i - 1, j, visited, heights[i][j])
+            dfs(i, j - 1, visited, heights[i][j])
             
             return
         
         for j in range(0, cols):
-            dfs(0, j, pacificSet, set(), -1)
-            dfs(rows - 1, j, atlanticSet, set(), -1)
+            dfs(0, j, pacificSet, heights[0][j])
+            dfs(rows - 1, j, atlanticSet, heights[rows-1][j])
         
         for i in range(0, rows):
-            dfs(i, 0, pacificSet, set(), -1)
-            dfs(i, cols - 1, atlanticSet, set(), -1)
+            dfs(i, 0, pacificSet, heights[i][0])
+            dfs(i, cols - 1, atlanticSet, heights[i][cols-1])
         
         return list(pacificSet.intersection(atlanticSet))
