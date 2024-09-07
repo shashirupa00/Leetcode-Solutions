80c407b05cca6edb9962bae921734d91c8692e7a
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-06 20:57:34 -0400

    Time: 129 ms (12.3%), Space: 18.5 MB (14.11%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,34 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
 
-        deq = collections.deque([])
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        maxArea = 0
+        res, area = 0, 0
 
+        def dfs(i, j):
+            
+            nonlocal area
+
+            if i not in range(rows) or j not in range(cols) or (i, j) in visited or not grid[i][j]:
+                return
+            
+            area += 1
+            visited.add((i, j))
+
+            dfs(i + 1, j)
+            dfs(i, j + 1)
+            dfs(i - 1, j)
+            dfs(i, j - 1)
+
+            return
+        
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 1:
+                if grid[i][j] and (i, j) not in visited:
                     area = 0
-                    deq.append((i,j))
-                    visited.add((i,j))
-                    while deq:
-                        x, y = deq.popleft()
-                        area += 1
-
-                        directions = [[0,1],[1,0],[0,-1],[-1,0]]
-
-                        for k in range(len(directions)):
-                            r, c = x+directions[k][0], y+directions[k][1]
-                             
-                            if (0 <= r <= rows-1 and
-                            0 <= c <= cols-1 and
-                            grid[r][c] == 1 and
-                            (r,c) not in visited):
-
-                                deq.append((r,c))
-                                visited.add((r,c))
-                    
-                    maxArea = max(maxArea, area)
+                    dfs(i, j)
+                    res = max(res, area)
         
-        return maxArea
+        return res
