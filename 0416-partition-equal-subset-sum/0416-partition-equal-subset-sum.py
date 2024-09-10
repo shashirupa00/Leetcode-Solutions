2315b998d5114ed4ea681da57390b404a15c5fca
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-10 10:57:01 -0400

    Time: 740 ms (44.96%), Space: 155.2 MB (16.2%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -2,28 +2,26 @@ class Solution:
     def canPartition(self, nums: List[int]) -> bool:
 
         totalSum = sum(nums)
-
-        if totalSum%2 == 1: return False
-
-        target = totalSum//2
-        sums = [0, nums[-1]]
-        visited = set(sums)
-
-        for i in range(len(nums)-2, -1, -1):
-            for j in range(len(sums)):
-                currSum = nums[i] + sums[j]
-
-                if currSum == target: return True
-
-                if currSum < target and currSum not in visited:
-                    sums.append(currSum)
-                    visited.add(currSum)
         
-        return False
+        if totalSum % 2:
+            return False
 
+        target = totalSum // 2
+        dp = {}
 
-                
+        def backTrack(i, sumSoFar):
+            
+            if sumSoFar > target or i >= len(nums):
+                return False
+            
+            if sumSoFar == target:
+                return True
+            
+            if (i, sumSoFar) in dp:
+                return dp[(i, sumSoFar)]
+            
+            dp[(i, sumSoFar)] = backTrack(i + 1, sumSoFar + nums[i]) or backTrack(i + 1, sumSoFar)
 
-
-
-
+            return dp[(i, sumSoFar)]
+                    
+        return backTrack(0, 0)
