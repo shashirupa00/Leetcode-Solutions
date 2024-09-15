d0d19ad03b44486e6b8d9f80136ee17d4934355b
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-15 12:41:45 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,37 +1,32 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-
-        hashSet, minHeap = {0}, []
+        
+        minHeap = []
         heapq.heapify(minHeap)
-        cost = point = 0
-
-        while len(hashSet) < len(points):
-            i = point
-            for j in range(len(points)):
-
-                if j in hashSet: continue
-
-                mDist = abs(points[i][0]-points[j][0]) + abs(points[i][1]- points[j][1])
-                heapq.heappush(minHeap, (mDist, j))
-            
-            while point in hashSet:
-                d, point = heapq.heappop(minHeap)
-
-            cost += d
-            hashSet.add(point)
+        res = 0
         
-        return cost
+        if len(points) <= 1:
+            return 0
 
-
-
-
-
-
-
-
-
-
-
-            
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                p1, p2 = points[i], points[j]
+                heapq.heappush(minHeap, (abs(p1[0] - p2[0]) + abs(p1[1] - p2[1]), i, j))
         
+        visited = set()
+        edges = 0
 
+        print(minHeap)
+
+        while edges + 1 != len(points) or len(visited) < len(points):
+
+            dist, i, j = heapq.heappop(minHeap)
+
+            if i not in visited or j not in visited or len(visited) == len(points):
+                res += dist
+                visited.add(i)
+                visited.add(j)
+                edges += 1
+                print(i, j, edges)
+
+        return res
