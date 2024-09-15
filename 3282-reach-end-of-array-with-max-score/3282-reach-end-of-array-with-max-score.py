7bd6cea0c3b634ba001c75fc58f3f61abdc9648d
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-09-15 07:53:42 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.py
+++ 3282-reach-end-of-array-with-max-score/3282-reach-end-of-array-with-max-score.py
@@ -1,10 +1,14 @@
 class Solution:
     def findMaximumScore(self, nums: List[int]) -> int:
         
-        res, maxJump = 0, 0
-
-        for jump in nums:
-            res += maxJump
-            maxJump = max(maxJump, jump)
-
-        return res
+        n = len(nums)
+        dp = [0] * n
+        
+        for i in range(n - 2, -1, -1):
+            max_score = 0
+            for j in range(i + 1, n):
+                score = (j - i) * nums[i] + dp[j]
+                max_score = max(max_score, score)
+            dp[i] = max_score
+        
+        return dp[0]
