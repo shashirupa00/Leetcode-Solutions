a21c7dcdf6574809aae8db490502828aeba4f799
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-10-16 10:44:16 -0700

    Time: 97 ms (27.62%), Space: 16.7 MB (21.32%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,23 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-
-        rows = collections.defaultdict(set)
-        cols = collections.defaultdict(set)
-        box = collections.defaultdict(set)
-
-        for i in range(9):
-            for j in range(9):
-                if board[i][j].isdigit():
-                    if (board[i][j] in rows[i] or
-                        board[i][j] in cols[j] or
-                        board[i][j] in box[(i//3, j//3)]):
-                            return False
-
-                    rows[i].add(board[i][j])
-                    cols[j].add(board[i][j])
-                    box[(i//3, j//3)].add(board[i][j])
-
-        return True
-
-                    
         
+        rows, cols = 9, 9
+        rowMap, colMap, boxMap = defaultdict(set), defaultdict(set), defaultdict(set)
+
+        for i in range(rows):
+            for j in range(cols):
+
+                if board[i][j] != '.':
+
+                    if board[i][j] in rowMap[i] or board[i][j] in colMap[j] or board[i][j] in boxMap[(i//3, j//3)]:
+                        return False
+                    
+                    rowMap[i].add(board[i][j])
+                    colMap[j].add(board[i][j])
+                    boxMap[(i//3, j//3)].add(board[i][j])
+        
+        return True
