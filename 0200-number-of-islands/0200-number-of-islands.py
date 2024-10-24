e410f1a38f0822600d8ce59f7f9ff707cd100f81
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-10-24 11:09:18 -0700

    Time: 300 ms (17.21%), Space: 28 MB (7.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,12 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        '''
+        empty grid is possible
+        even dimensions
+        only 1s and 0s but strings
+        no diagonal neighbhors 
+
+        input: mxn 2d array with 1s and 0s
+        output: integer
+        '''
+
         rows, cols = len(grid), len(grid[0])
         res = 0
         visited = set()
 
         def dfs(i, j):
-
+            
             if i not in range(rows) or j not in range(cols) or (i, j) in visited or grid[i][j] == '0':
                 return
             
@@ -17,12 +27,12 @@ class Solution:
             dfs(i - 1, j)
             dfs(i, j - 1)
 
-            return
-        
+            return 
+            
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == '1' and (i, j) not in visited:
-                    res += 1
                     dfs(i, j)
+                    res += 1
         
         return res
