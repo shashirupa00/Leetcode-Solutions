e01047642f1cdd305494a8e2f8e3ff5dd476a7ea
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-10-25 11:02:06 -0700

    Time: 5 ms (94.35%), Space: 18.7 MB (16.77%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,37 +1,37 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        
+
         hashMap = defaultdict(list)
-
-        for dst, src in prerequisites:
-            hashMap[src].append(dst)
-
         visited = set()
         res = []
 
-        def dfs(node, cycle):
+        for src, dst in prerequisites:
+            hashMap[src].append(dst)
+
+        
+        def dfs(course, cycle):
             
-            if node in cycle:
+            if course in cycle:
                 return False
             
-            if node in visited:
+            if course in visited:
                 return True
+            
+            visited.add(course)
+            cycle.add(course)
 
-            visited.add(node)
-            cycle.add(node)
-
-            for nxt in hashMap[node]:
+            for nxt in hashMap[course]:
                 if not dfs(nxt, cycle):
                     return False
             
-            res.append(node)
-            cycle.remove(node)
+            res.append(course)
+            cycle.remove(course)
 
             return True
-
+        
         for i in range(numCourses):
             if i not in visited:
                 if not dfs(i, set()):
                     return []
         
-        return res[::-1]
+        return res if len(visited) == numCourses else []
