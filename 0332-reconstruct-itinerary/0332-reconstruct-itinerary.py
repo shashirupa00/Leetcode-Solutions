f24a23d4c4c44e6786152091320655b5d7ef4439
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-10-26 16:54:32 -0700

    Time: 3 ms (76.19%), Space: 16.9 MB (44.93%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -1,35 +1,20 @@
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-
-        res = ["JFK"]
-        hashMap = collections.defaultdict(collections.deque)
-
-        tickets.sort()
-
-        for fro, to in tickets:
-            hashMap[fro].append(to)
         
-        def dfs(vertex):
+        graph = defaultdict(list)
 
-            nonlocal res
+        for src, dst in tickets:
+            graph[src].append(dst)
 
-            if len(res) == len(tickets)+1:
-                return True
-            if vertex not in hashMap:
-                return False
-            
-            temp = list(hashMap[vertex])
-
-            for i in range(len(temp)):
-                curr = hashMap[vertex].popleft()
-                res.append(curr)
-
-                if dfs(curr): return True
-
-                res.pop()
-                hashMap[vertex].append(curr)
-                       
-            return False
+        for src in graph:
+            graph[src].sort(reverse=True) 
+        
+        itinerary = []
+        
+        def dfs(airport):
+            while graph.get(airport, []):
+                dfs(graph[airport].pop())
+            itinerary.append(airport)
         
         dfs("JFK")
-        return res        
+        return itinerary[::-1]    
