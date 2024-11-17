7520812596ad93188fa7eb11837c4859c556044a
Shashi Shirupa <30957500+shashirupa00@users.noreply.github.com>
2024-11-17 10:57:23 -0800

    Time: 909 ms (5.09%), Space: 31.2 MB (64.1%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -3,24 +3,29 @@ class Solution:
         
         arr = []
         prefix = 0
+        deq = collections.deque([])
         res = float("inf")
 
-        for i, num in enumerate(nums):
+        for num in nums:
             prefix += num
-            arr.append((prefix, i))
-        
-        deq = collections.deque([])
+            arr.append(prefix)
 
-        for num, i in arr:
+        for i, num in enumerate(arr):
 
-            while deq and deq[-1][0] <= num:
+            while deq and deq[-1][0] > num:
                 deq.pop()
             
             deq.append((num, i))
-            if num >= k: res = min(res, i + 1)
+            if num >= k: res = min(res, i+1)
 
             while deq and num - deq[0][0] >= k:
                 _, idx = deq.popleft()
-                res = min(res, i - idx + 1)
-
+                res = min(res, i-idx)
+        
         return res if res != float("inf") else -1
+
+
+
+
+        
+        
